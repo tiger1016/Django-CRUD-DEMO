0602a80e5de5506bac25a84efbe01b096ebec64f
jackson <applejuicy1016@gmail.com>
2019-12-23 07:59:00 -0800

    last commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'posts',
     'pages',
+    'blog',
+    'catalog',
 ]
 
 MIDDLEWARE = [
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include 
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pages/', include("pages.urls")),
     path('posts/', include("posts.urls")),
+    path('', RedirectView.as_view(url='admin/'))
 ]
